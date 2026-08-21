433c486cc27a17901a73ad77876bc58c2e5b13a4
Gabriel Scherer <gabriel.scherer@gmail.com>
2026-08-21 21:17:53 +0200

    minor ml-schedule style change

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ set -x
 
 pandoc --standalone announce.md > docs/index.html
 pandoc --standalone ml2026-schedule.md > docs/ml2026-schedule.html
-
+sed -i 's/max-width: 36em;//g' docs/ml2026-schedule.html
 
